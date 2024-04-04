136a52622a0454fbcc8d365236922e4500660ddf
Abilash Ganesamoorthy <43057892+GanesamoorthyAbilash@users.noreply.github.com>
2024-04-04 11:42:36 +0800

    Update app.c

--- Pingpong/app.c
+++ Pingpong/app.c
@@ -1,21 +1,7 @@
-//PING PONG with 10240
-
-/***************************************************************************//**
- * @file
- * @brief Top level application functions
- *******************************************************************************
- * # License
- * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
- *******************************************************************************
- *
- * The licensor of this software is Silicon Laboratories Inc. Your use of this
- * software is governed by the terms of Silicon Labs Master Software License
- * Agreement (MSLA) available at
- * www.silabs.com/about-us/legal/master-software-license-agreement. This
- * software is distributed to you in Source Code format and is governed by the
- * sections of the MSLA applicable to Source Code.
- *
- ******************************************************************************/
+// Reference Programs:
+//https://github.com/SiliconLabs/peripheral_examples/tree/master/series2/iadc/iadc_scan_iadc_timer
+//Drivers from Simplicity Studio 5 gecko SDK 4.4.1
+//PG28 Pro Kit Example Project ==> Platform - I/O Stream USART Bare-metal
 
 #include <app_peripherals.h>
 #include <stdio.h>
